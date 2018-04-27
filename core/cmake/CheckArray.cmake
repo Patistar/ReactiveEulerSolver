6aabcbb09838338587e567addb08a4d8fc68f847
Maikel Nadolski <maikel.nadolski@gmail.com>
2018-04-27 05:49:58 +0000

    Fix library order in CMakeLists for linux

--- core/cmake/CheckArray.cmake
+++ core/cmake/CheckArray.cmake
@@ -1,18 +1,17 @@
 get_filename_component(_mod_dir ${CMAKE_CURRENT_LIST_FILE} PATH)
-message(STATUS "Checking for constexpr-enabled std::array...")
+message(STATUS "Checking for constexpr std::array...")
 try_compile(_has_constexpr_std_array
   ${CMAKE_BINARY_DIR}
   ${_mod_dir}/CheckStdArray.cpp
   CXX_STANDARD ${FUB_CORE_CXX_STANDARD})
-if (_has_std_array)
-  message(STATUS "Checking for constexpr-enabled std::array... Success")
+if (_has_constexpr_std_array)
+  message(STATUS "Checking for constexpr std::array... Success")
 else()
-  message(STATUS "Checking for constexpr-enabled std::array... Failure")
+  message(STATUS "Checking for constexpr std::array... Failure")
 endif()
 
 set(FUB_CORE_USE_STD_ARRAY _has_constexpr_std_array)
 mark_as_advanced(FUB_CORE_USE_STD_ARRAY)
 
 unset(_mod_dir)
-unset(_has_std_variant)
-unset(_has_std_experimental_variant)
+unset(_has_std_experimental_array)
